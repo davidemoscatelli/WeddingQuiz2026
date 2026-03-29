2cca764601511989d4c23e68fd4bf3894b4b5c86
Davide Moscatelli <40373072+davidemoscatelli@users.noreply.github.com>
2026-03-29 14:52:20 +0200

    fix allowed hosts

--- wedding_backend/settings.py
+++ wedding_backend/settings.py
@@ -28,8 +28,9 @@ SECRET_KEY = 'django-insecure-(23eziih89gos1nxojp7aj-gv1qvipc$4km)&#vaz^rkb3u^($
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+if 'RENDER_EXTERNAL_HOSTNAME' in os.environ:
+    ALLOWED_HOSTS.append(os.environ.get('RENDER_EXTERNAL_HOSTNAME'))
 
 # Application definition
 
