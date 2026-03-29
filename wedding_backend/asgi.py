ba2b180b9e5976ca2db1dbc881fcb8b69d825b67
Davide Moscatelli <40373072+davidemoscatelli@users.noreply.github.com>
2026-03-29 14:45:39 +0200

    asgi

--- wedding_backend/asgi.py
+++ wedding_backend/asgi.py
@@ -1,13 +1,19 @@
 import os
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import quiz_engine.routing # Lo creeremo tra un attimo
 
+# 1. Imposta le variabili d'ambiente PRIMA di tutto il resto
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wedding_backend.settings')
 
+# 2. Inizializza l'applicazione Django ASGI
+django_asgi_app = get_asgi_application()
+
+# 3. Importa il resto solo dopo che Django è stato inizializzato
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import quiz_engine.routing
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             quiz_engine.routing.websocket_urlpatterns
