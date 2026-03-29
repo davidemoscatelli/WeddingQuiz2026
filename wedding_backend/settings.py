2370761bd0c42e37ab3a62b1e6b1d796e9688bc3
Davide Moscatelli <40373072+davidemoscatelli@users.noreply.github.com>
2026-03-29 14:33:43 +0200

    fix

--- wedding_backend/settings.py
+++ wedding_backend/settings.py
@@ -128,6 +128,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
