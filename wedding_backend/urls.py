7dd5ed75c5bc5f753f570ca86c806ef5e4a0e40d
Davide Moscatelli <40373072+davidemoscatelli@users.noreply.github.com>
2026-03-29 15:38:19 +0200

    fix urls

--- wedding_backend/urls.py
+++ wedding_backend/urls.py
@@ -5,7 +5,8 @@ from django.contrib.auth import views as auth_views # Importiamo la nostra vista
 
 urlpatterns = [
     path('admin/', admin.site.urls), # Questo è l'admin classico (per inserire le canzoni prima del matrimonio)
-    path('regia/', views.telecomando, name='telecomando'), # Questo è il tuo telecomando da smartphone!
+    path('', views.telecomando, name='telecomando'), # Questo è il tuo telecomando da smartphone!
     path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    
 ]
